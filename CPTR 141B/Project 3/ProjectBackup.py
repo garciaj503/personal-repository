f2285def0a64e231f77484892b34e2d8e8adad70
jaime <jagarcia767.jr@gmail.com>
2023-11-29 22:17:38 -0800

    Wednesday advance

--- CPTR 141B/Project 3/ProjectBackup.py
+++ CPTR 141B/Project 3/ProjectBackup.py
@@ -7,12 +7,12 @@ not_switch_games = 0
 switch_games = 0
 not_switch_wins = 0
 switch_wins = 0
-
-
+switch_loss = 0
+not_switch_loss = 0
                     
-#Define all the functions that I will use
 random.seed(input("Enter a seed: ")) #This feels kinda uselesss
 
+#Define all the functions that I will use
 def menu(): #Overview of the program
     print("""Menu:
   1) Play a live game
@@ -22,23 +22,25 @@ def menu(): #Overview of the program
 def LiveGame(): #Overview of the simulation
     print("""
 There are three doors, each contains a prize
-behind one door there is $10,000 hidden,
-behind the other two there is dishwasher detergent.
+behind one door there is a Macbook Pro with an M3 chip,
+behind the other two you will find the cases for a Macbook Pro.
 Which door will you choose: 1, 2, or 3?          
 """)
 
 def SimulatedGames():
-    simulations = input('How many times should the game be simulated? ')
+    print("""Welcome to the simulated version of the game.
+Here, the machine will do everything for you""")
+    simulations = input('How many times would you wish to simulate this game? ')
     
     #Checking if user entered a valid choice
     while not simulations.isdigit():
         print("Please enter a valid number of simulations")
         print()
-        simulations = input('How many times should the game be simulated? ')
+        simulations = input('How many times would you wish to simulate this game? ')
     return simulations
 
 def OtherDoorFrom(doorA, doorB):
-    doors2 = doors1[:] #Making doors1 global to be able to modify it
+    doors2 = doors1[:] #Creating a copy of doors1 to create a new set of doors each time
     if doorA != doorB: #If parameters are not equal, I remove both parameters from the list
         doors2.remove(doorA)
         doors2.remove(doorB)
@@ -47,7 +49,7 @@ def OtherDoorFrom(doorA, doorB):
     return doors2[0] #Returning the first value of doors1, in one case the list will only have one value.
 
 def SwitchDoorFrom(doorA, doorB):
-    doors2 = doors1[:] #Making doors1 global to be able to modify it
+    doors2 = doors1[:] #Creating a copy of doors1 to create a new set of doors each time
     if doorA != doorB:
         doors2.remove(doorA)
         return doorB
@@ -55,13 +57,17 @@ def SwitchDoorFrom(doorA, doorB):
         doors2.remove(doorA)
         return doors2[-1]
     
-def EndGame():
-    total_games = switch_games + not_switch_games
-    total_wins = switch_wins + not_switch_wins
-    switch_win_rate = f'{((switch_wins / switch_games) * 100):.1f}%'
-    not_switch_win_rate = F'{((not_switch_wins/not_switch_games)*100):.1f}%'
-    total_win_rate = f'{((total_wins / total_games) * 100):.1f}%'
-    print(f"{"Strategy":^16}|{"Games":^16}|{"Win":^16}|{"Win Rate":^16}")
+def EndGame(UserChoice):
+    if UserChoice == '3' and not_switch_games == 0:
+        switch_games = 0
+    else:  
+        total_games = switch_games + not_switch_games
+        total_wins = switch_wins + not_switch_wins
+        total_loss = switch_loss + not_switch_loss 
+        switch_win_rate = f'{((switch_wins / switch_games) * 100):.1f}%'
+        not_switch_win_rate = F'{((not_switch_wins / not_switch_games) * 100):.1f}%'
+        total_win_rate = f'{((total_wins / total_games) * 100):.1f}%'
+    print(f"{"Strategy":^16}|{"Games":^16}|{"Wins":^16}|{"Win Rate":^16}")
     print(f"{'-' * 64}")
     print(f"{"Switch":^16}|{switch_games:^16}|{switch_wins:^16}|{switch_win_rate:^16}")
     print(f"{"Don't switch":^16}|{not_switch_games:^16}|{not_switch_wins:^16}|{not_switch_win_rate:^16}")
@@ -95,14 +101,14 @@ def main():
             #Checking if the user entered a valid choice
             while door_choice not in ('123'):
                 print()
-                door_choice = input("C'mon bro, choose a door, 1, 2, or 3? ")
+                door_choice = input("C'mon bro, just choose a door, 1, 2, or 3? ")
                 
             print()
             
             #Giving the user the chance to switch door by revealing one of them
             print(f"""Ok let's calm down a bit. I want to tell you that behind door {OtherDoorFrom(door_choice, prizeDoor)},
-I have hidden laundry detergent, and you chose door {door_choice}.
-Your choice might be right and win $10,000, but who knows what could happen. 
+there's one of the two cases for a Macbook, and you chose door {door_choice}.
+Your choice might be right and win the latest Macbook Pro, but who knows what could happen. 
 So, I will give you the chance to switch from door {door_choice} to door {SwitchDoorFrom(door_choice, prizeDoor)}
 Do you accept the deal?        
 """)
@@ -117,27 +123,31 @@ Do you accept the deal?
             if switch_choice == 'y':
                 global switch_games
                 global switch_wins
+                global switch_loss
                 switch_games += 1
                 new_choice = SwitchDoorFrom(door_choice, prizeDoor) #Assigning the new user choice
                 if new_choice == prizeDoor:
                     switch_wins += 1
-                    print("You were wise by switching doors. You just won $10,000. Congratulations!")
+                    print("You were wise by switching doors. You just won a Macbook Pro! Congratulations!")
                     print()
                 else:
-                    print(f"Well, that's too bad. The prize was behind door {prizeDoor}")
+                    switch_loss += 1
+                    print("Well, you won the case, you just need the Macbook now. Good luck next time!")
                     print()
             
             #If the user did not decide to switch his door choice
             elif switch_choice == 'n':
                 global not_switch_games
                 global not_switch_wins
+                global not_switch_loss
                 not_switch_games += 1
                 if door_choice == prizeDoor: #User choice stays the same since he did not want to switch
                     not_switch_wins += 1
-                    print("You were wise by not switching doors. You just won $10,000. Congratulations!")
+                    print("You were wise by switching doors. You just won a Macbook Pro! Congratulations!")
                     print()
                 else:
-                    print(f"Well, that's too bad. The prize was behind door {prizeDoor}")
+                    not_switch_loss += 1
+                    print("Well, you won the case, you just need the Macbook now. Good luck next time!")
                     print()
                                    
         elif user_choice == '2':
@@ -148,7 +158,6 @@ Do you accept the deal?
             for i in range(int(simulations)):
                 prizeDoor = random.choice(doors) #Prize is randomly hidden
                 userDoor = random.choice(doors) #User choice is randomly selected
-                # DoorToReveal = OtherDoorFrom(userDoor, prizeDoor)
                 DoorToSwitch = SwitchDoorFrom(userDoor, prizeDoor)
                 SwitchChoice = random.choice(switch_options)
                 if SwitchChoice == 'y':
@@ -156,18 +165,23 @@ Do you accept the deal?
                     userDoor = DoorToSwitch
                     if userDoor == prizeDoor:
                         switch_wins += 1
-                        
+                    else:
+                        switch_loss += 1
+                               
                 elif SwitchChoice == 'n':
                     not_switch_games += 1
                     if userDoor == prizeDoor:
-                        not_switch_wins += 1    
+                        not_switch_wins += 1
+                    else:
+                        not_switch_loss += 1    
                         
             print(f'{simulations} simulations done. Results will display at the end of the game')
             print()
                    
         elif user_choice == '3':
-            EndGame()
+            EndGame(user_choice)
+            print()
+            print("Thanks for playing!")
             break
-        
-                    
+                               
 main()
