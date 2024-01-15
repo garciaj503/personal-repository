707069c7854ba6394f5e3fed25295906f32bcb35
jaime <jagarcia767.jr@gmail.com>
2024-01-15 08:28:56 -0800

    Last commit before moving my repository

--- Personal Test Files/LeetCode/IsPalindrome.py
+++ Personal Test Files/LeetCode/IsPalindrome.py
@@ -1,12 +1,9 @@
 def isPalindrome(x):
     y = str(x)
     z = y[::-1]
-    print(type(z))
-    print(type(x))
     if str(x) == z:
         return True
     else:
         return False
     
 print(isPalindrome(121))
-isPalindrome(121)
