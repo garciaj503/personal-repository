a283e83cb5894e6e3ca4520510b8b80b683c989d
jaime <jagarcia767.jr@gmail.com>
2024-01-24 23:29:05 -0800

    Adding Project 1 of CPTR 142

--- CPTR 142A/Project 1/game_simulation.py
+++ CPTR 142A/Project 1/game_simulation.py
@@ -2,30 +2,18 @@
 import random
 import classes
 
-seed = input("Please enter a seed: ")
+print("Welcome to the LCR game!")
+seed = input("Please enter a seed to start the game: ")
 
 random.seed(seed)
 
 num_players = input("Please enter the number of players: ")
 
 #Checking for a valid  numeric input
-while not num_players.isnumeric():
-    print("Please enter a valid number.")
-    print()
-    num_players = input("Try again: ")
-
-num_players = int(num_players)
+num_players = classes.numeric_num_players_checking(num_players)
 
 #Checking if user entered at least 3 players
-while num_players < 3:
-    print("Please enter 3 or more players")
-    print()
-    num_players = input("Plase enter the number of players: ")
-    while not num_players.isnumeric():
-        print("Please enter a valid number.")
-        print()
-        num_players = input("Try again: ")
-    num_players = int(num_players)
+num_players = classes.num_players_checking(num_players)
 
 print()
 
@@ -47,49 +35,35 @@ print()
 num_tokens = input("Enter the number of tokens each player gets to start with (no less than 3): ")
 
 #Checking for a valid  numeric input
-while not num_tokens.isnumeric():
-    print("Please enter a valid amount of tokens")
-    print()
-    num_tokens = input("Enter the number of tokens each player gets to start with (no less than 3): ")
+num_tokens = classes.numeric_token_checking(num_tokens)
 
-num_tokens = int(num_tokens)
-
-#Checking if user input is less than 3, then ask again for a number greater than or equal to 3
-if num_tokens < 3:
-    while num_tokens < 3:
-        print("The amount of tokens has to be greater than or equal to 3.")
-        print()
-        num_tokens = input("Try Again: ")
-        while not num_tokens.isnumeric():
-            print("Please enter a valid amount of tokens")
-            print()
-            num_tokens = input("Enter the number of tokens each player gets to start with (no less than 3): ")
-        num_tokens = int(num_tokens)
+#Checking if user entered at least 3 tokens
+num_tokens = classes.num_token_checking(num_tokens)
 
 player_classes = [classes.Player(i, num_tokens) for i in player_names]
 
+#Simulating the game
 while any(player.tokens for player in player_classes):
-
     for index, object1 in enumerate(player_classes):
         sides_list = object1.roll_dice()
         object1.turns += 1
 
+        #If the player has 0 tokens, then their turn is skipped
         if object1.tokens == 0:
             object1.turns_skipped += 1
+            #Player out of the game if they haven't played for two rounds
             if object1.turns_skipped == 2:
                 del player_classes[index]
             continue
+
+        #Checking for each outcome of the dice and performing the required process
         for side in sides_list:
             if side == "L":
                 object1.tokens -= 1
                 player_classes[index - 1].tokens += 1
             elif side == "R":
-                index_last_player = len(player_classes) - 1
                 object1.tokens -= 1
-                if index == index_last_player:
-                    player_classes[0].tokens += 1
-                else:
-                    player_classes[index + 1].tokens += 1
+                player_classes[(index + 1) % len(player_classes)].tokens += 1
             elif side == "C":
                 object1.tokens -= 1
 
