5c4607a798635d7dd838484aae4384b266875f0c
jagarcia767.jr@gmail.com <garciaj503>
2024-03-03 14:57:28 -0800

    Modifying testing.cpp file

--- CPTR 142A/Project 2/testing.cpp
+++ CPTR 142A/Project 2/testing.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
+#include "functions.h"
 #include <cassert>
-#include "headers.h"
+#include <iostream>
 
 int main() {
 
-	std::cout << "Testing started." << std::endl;
-	//Testing function OtherDoorFrom
-	assert(OtherDoorFrom(1, 1) == 2);
-	assert(OtherDoorFrom(1, 2) == 3);
-	assert(OtherDoorFrom(1, 3) == 2);
-	assert(OtherDoorFrom(2, 1) == 3);
-	assert(OtherDoorFrom(2, 2) == 1);
-	assert(OtherDoorFrom(2, 3) == 1);
-	assert(OtherDoorFrom(3, 1) == 2);
-	assert(OtherDoorFrom(3, 2) == 1);
-	assert(OtherDoorFrom(3, 3) == 1);
+  std::cout << "Testing started." << std::endl;
+  // Testing function OtherDoorFrom
+  assert(OtherDoorFrom(1, 1) == 2);
+  assert(OtherDoorFrom(1, 2) == 3);
+  assert(OtherDoorFrom(1, 3) == 2);
+  assert(OtherDoorFrom(2, 1) == 3);
+  assert(OtherDoorFrom(2, 2) == 1);
+  assert(OtherDoorFrom(2, 3) == 1);
+  assert(OtherDoorFrom(3, 1) == 2);
+  assert(OtherDoorFrom(3, 2) == 1);
+  assert(OtherDoorFrom(3, 3) == 1);
 
-	//Testing function SwitchDoorTo
-	assert(SwitchDoorTo(1, 1) == 3);
-	assert(SwitchDoorTo(1, 2) == 2);
-	// assert(SwitchDoorTo(1, 3) == 2);
-	// assert(SwitchDoorTo(2, 1) == 3);
-	// assert(SwitchDoorTo(2, 2) == 3);
-	// assert(SwitchDoorTo(2, 3) == 1);
-	// assert(SwitchDoorTo(3, 1) == 2);
-	// assert(SwitchDoorTo(3, 2) == 1);
-	// assert(SwitchDoorTo(3, 3) == 2);
+  // Testing function SwitchDoorTo
+  assert(SwitchDoorTo(1, 1) == 3);
+  assert(SwitchDoorTo(1, 2) == 2);
+  assert(SwitchDoorTo(1, 3) == 3);
+  assert(SwitchDoorTo(2, 1) == 1);
+  assert(SwitchDoorTo(2, 2) == 3);
+  assert(SwitchDoorTo(2, 3) == 3);
+  assert(SwitchDoorTo(3, 1) == 1);
+  assert(SwitchDoorTo(3, 2) == 2);
+  assert(SwitchDoorTo(3, 3) == 2);
 
-	std::cout << "..." << std::endl;
-	std::cout << "Testing finished. 0 errors" << std::endl;
+  std::cout << "..." << std::endl;
+  std::cout << "Testing finished. 0 errors" << std::endl;
 
   return 0;
 }
